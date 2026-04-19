70c3ae1f85fa6df94adf2b7c83c40c76f13f8a9e
HarelValfish <harelvalfish@gmail.com>
2026-04-19 11:18:25 +0300

    Aassimilated HTML file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes import tasks_bp
 from errors import errors_bp
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 app.register_blueprint(tasks_bp)
 app.register_blueprint(errors_bp)
+
+@app.route("/", methods = ["GET"])
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
